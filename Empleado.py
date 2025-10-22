0744211b9f50bd06af11350537471b67e4109e6d
Cristian Elizondo Cascante <cris.elizondo.cascante@gmail.com>
2025-10-22 14:20:58 -0600

    probando

--- Empleado.py
+++ Empleado.py
@@ -9,3 +9,13 @@ class Empleado:
 
     def __str__(self):
         return f"{self.id} - {self.nombre} {self.apellido}, {self.edad} años, Telefono: {self.telefono}, Email: {self.correo}"
+    
+    #recursividad para definir el tipo de contrato
+    def tipo_contrato(self, opcion):
+        if opcion == 1:
+           self.tipo_contrato = "Contrato por horas"
+        elif opcion == 2:
+           self.tipo_contrato = "Contrato mensual"
+        else:
+           self.tipo_contrato = "Tipo de contrato no válido"
+
