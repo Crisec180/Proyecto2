041b94b92bdab711ca92bbf2a4467538c087e8d5
Nashi <sebasgarro2006@gmail.com>
2025-10-24 21:30:11 -0600

    Hice lo de pilas (NetoXContrato y NetoXHoras)

--- CalculosPILAS/CalcularNetoXContrato.py
+++ CalculosPILAS/CalcularNetoXContrato.py
@@ -1,16 +1,42 @@
 # CalculosPilas/CalcularNetoXContrato.py
 from CalculosConDICCIONARIOS import CalcularDeduccionNormal
 from CalculosConDICCIONARIOS import CalculaOtrasDeducciones
+import PilasParaCalculos as Pila
+
+# Diccionario de porcentajes de deducción según tipo de pago
+PORCENTAJE_PAGO = {
+    "Semanal": 0.08,
+    "Quincenal": 0.12
+}
 
 class CalcularNetoXContrato:
-    def __init__(self, empleado,deducciones):
-        self.empleado = empleado
-        self.deducciones = deducciones
+    def __init__(self):
+        self.pila = Pila.Pila()
 
-    def calcular_neto_por_contrato(self):
+    def calcular_y_guardar(self, empleado):
+        porcentaje = PORCENTAJE_PAGO.get(empleado.tipo_contrato, 0)
+        salario_bruto = float(empleado.salario_base)
+        ajuste = salario_bruto * porcentaje
+        deducciones_normales = CalcularDeduccionNormal(empleado, salario_bruto)
+        otras_deducciones = CalculaOtrasDeducciones(empleado, salario_bruto - deducciones_normales - ajuste)
+        neto = salario_bruto - ajuste - deducciones_normales - otras_deducciones
 
-        deduccion_normal = CalcularDeduccionNormal.CalcularDeduccionNormal(self.empleado, self.deducciones)
-        otras_deducciones = CalculaOtrasDeducciones.CalculaOtrasDeducciones(self.empleado, self.deducciones)
+        resultado = {
+            "id": empleado.id,
+            "nombre": empleado.nombre,
+            "tipo_contrato": empleado.tipo_contrato,
+            "salario_bruto": salario_bruto,
+            "porcentaje_periodo": porcentaje,
+            "ajuste": ajuste,
+            "deducciones_normales": deducciones_normales,
+            "otras_deducciones": otras_deducciones,
+            "neto": neto
+        }
+        self.pila.push(resultado)
+        return resultado
 
-        neto = self.empleado.salario_base - deduccion_normal.calcular_deduccion() - otras_deducciones.calcular_deduccion()
-        return neto
+    def procesar(self):
+        return self.pila.pop()
+
+    def mostrar_pila(self):
+        self.pila.mostrar()
--- CalculosPILAS/ObtenerNetoXHoras.py
+++ CalculosPILAS/ObtenerNetoXHoras.py
@@ -1,14 +1,43 @@
 from CalculosConDICCIONARIOS import CalculaBrutoXHora
 from CalculosConDICCIONARIOS import CalcularDeduccionNormal
+import PilasParaCalculos as Pila
+
+# Diccionario de bonos por departamento
+BONO_DEPARTAMENTO = {
+    "Ingeniería": 0.5,
+    "Administración": 0.3,
+    "Ventas": 0.4
+}
 
 class ObtenerNetoXHoras:
-    def __init__(self, empleado, horas_trabajadas, valor_hora):
-        self.horas_trabajadas = horas_trabajadas
-        self.valor_hora = valor_hora
+   def __init__(self):
+        self.pila = Pila.Pila()
 
-    #segun estiquetas de la parte grafica es un valor o otro
-    #ejemplo si es Ingenieria le supa 0.5 
-    #ventas 0.2 y  administracion 0.3
-    def calcular_neto(self, empleado):
-        if "Ingenieria":
-            ""
+def calcular_y_guardar(self, empleado, horas_trabajadas, tarifa_hora, deducciones_extra=0):
+    bono = BONO_DEPARTAMENTO.get(empleado.departamento, 0)
+    bruto = CalculaBrutoXHora(empleado, horas_trabajadas, tarifa_hora)
+    bruto_bono = bruto * (1 + bono)
+    deducciones_normales = CalcularDeduccionNormal(empleado, bruto_bono)
+    neto = bruto_bono - deducciones_normales - deducciones_extra
+
+    resultado = {
+        "id": empleado.id,
+        "nombre": empleado.nombre,
+        "departamento": empleado.departamento,
+        "horas_trabajadas": horas_trabajadas,
+        "tarifa_hora": tarifa_hora,
+        "bono_departamento": bono,
+        "bruto": bruto,
+        "bruto_con_bono": bruto_bono,
+        "deducciones_normales": deducciones_normales,
+        "deducciones_extra": deducciones_extra,
+        "neto": neto
+    }
+    self.pila.push(resultado)
+    return resultado
+
+def procesar(self):
+    return self.pila.pop()
+
+def mostrar_pila(self):
+    self.pila.mostrar()
